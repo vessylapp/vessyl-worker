d8ae6bcdfb0d0d8b1427ea1e520c103e94245ca4
Ben Thomas <benjamint08@gmail.com>
2024-06-01 17:04:34 +0100

    Add JWT ready for UI i guess

--- src/users/create.ts
+++ src/users/create.ts
@@ -1,8 +1,19 @@
 import {Hono} from "hono";
 import MongoService from "../../structures/mongodb";
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const app = new Hono();
+
+function generateRandomString(length: number) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 
 app.post('/', async (c) => {
     const data = await c.req.text();
@@ -26,7 +37,13 @@ app.post('/', async (c) => {
         customData.admin = true;
     }
     await client.insert('vessyl', 'users', {username, password: hashedPassword , ...customData});
-    return c.text('User created');
+    let jwtSecret = await client.findOne('vessyl', 'settings', {jwtSecret: {$exists : true}});
+    if (!jwtSecret) {
+        await client.insert('vessyl', 'settings', {jwtSecret: generateRandomString(20)});
+        jwtSecret = await client.findOne('vessyl', 'settings', {jwtSecret: {$exists : true}});
+    }
+    const token = jwt.sign({username}, jwtSecret.jwtSecret);
+    return c.json({token});
 });
 
 export default app;
