387eda4fae7541413f3b7c949e38b14fc7bc1152
Ben Thomas <benjamint08@gmail.com>
2024-06-01 17:48:49 +0100

    Login route

--- src/users.ts
+++ src/users.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
 import { exec } from 'child_process'
 import newUser from './users/create'
+import login from './users/login'
 
 const app = new Hono()
 
 app.route('/create', newUser)
+app.route('/login', login)
 
 export default app
---
+++ src/users/login.ts
@@ -0,0 +1,40 @@
+import {Hono} from "hono";
+import MongoService from "../../structures/mongodb";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const app = new Hono();
+
+function generateRandomString(length: number) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+app.post('/', async (c) => {
+    const data = await c.req.text();
+    const body = JSON.parse(data);
+    const client = MongoService.getInstance();
+    const {username, password} = body;
+    const userExists = await client.findOne('vessyl', 'users', {username});
+    if (!userExists) {
+        return c.text('User not found');
+    }
+    const passwordIsValid = await bcrypt.compare(password, userExists.password);
+    if (!passwordIsValid) {
+        return c.text('Invalid password');
+    }
+    let jwtSecret = await client.findOne('vessyl', 'settings', {jwtSecret: {$exists : true}});
+    if (!jwtSecret) {
+        await client.insert('vessyl', 'settings', {jwtSecret: generateRandomString(20)});
+        jwtSecret = await client.findOne('vessyl', 'settings', {jwtSecret: {$exists : true}});
+    }
+    const token = jwt.sign({username}, jwtSecret.jwtSecret);
+    return c.json({token});
+});
+
+export default app;
