1db6302c70b9bac778e84340060f14a2b0f5ce4b
Ben Thomas <benjamint08@gmail.com>
2024-06-07 15:54:24 +0100

    0.0.20, updater works

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import containers from './containers'
 import users from './users'
 import status from './status'
 import resources from './resources'
+import update from './update'
 import { checkForUpdates } from '../structures/checkforupdates'
 
 import dotenv from 'dotenv'
@@ -37,6 +38,7 @@ async function startApp() {
   app.route('/users', users);
   app.route('/status', status);
   app.route('/resources', resources)
+  app.route('/update', update)
 
   const port = 8000
   console.log(`Server is running on port ${port}`)
---
+++ src/update.ts
@@ -0,0 +1,41 @@
+import { Hono } from 'hono'
+import { exec } from 'child_process'
+import jwt from 'jsonwebtoken'
+import MongoService from '../structures/mongodb'
+
+const app = new Hono()
+
+app.post('/', async (c) => {
+    const data = await c.req.text(); 
+    const body = JSON.parse(data);
+    const {token} = body;
+    const client = MongoService.getInstance();
+    const jwtSecret = await client.findOne('vessyl', 'settings', {jwtSecret: {$exists : true}});
+    if (!jwtSecret) {
+        return c.text('JWT Secret not found');
+    }
+    let decoded : any = {};
+    try {
+        decoded = jwt.verify(token, jwtSecret.jwtSecret);
+    } catch (err) {
+        return c.text('Invalid token');
+    }
+    const user = await client.findOne('vessyl', 'users', {username: decoded.username});
+    if (!user) {
+        return c.text('User not found');
+    }
+    if(user.admin === false || user.admin === undefined) {
+        return c.json({error: 'User is not an admin'});
+    }
+    const command = `docker run --rm --pull always -d -v /var/run/docker.sock:/var/run/docker.sock ghcr.io/vessylapp/vessyl-updater:latest`
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error || stderr.includes('Error') === true) {
+                return resolve(c.json({error: `${stderr}`}));
+            }
+            resolve(c.json({success: "Updater started successfully"}))
+        })
+    })
+})
+
+export default app
