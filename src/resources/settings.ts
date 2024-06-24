6e7db1c0fe60a1df483498a5af9322a20cffec80
Ben Thomas <benjamint08@gmail.com>
2024-06-24 15:03:35 +0100

    0.0.39 added proxy

--- src/resources/settings.ts
+++ src/resources/settings.ts
@@ -7,7 +7,7 @@ const app = new Hono()
 app.post('/', async (c) => {
     const data = await c.req.text(); 
     const body = JSON.parse(data);
-    const {token, name, git_url, type, env, ports, network, volumes} = body;
+    const {token, name, git_url, type, env, ports, network, volumes, domain} = body;
     const client = MongoService.getInstance();
     const jwtSecret = await client.findOne('vessyl', 'settings', {jwtSecret: {$exists : true}});
     if (!jwtSecret) {
@@ -46,7 +46,47 @@ app.post('/', async (c) => {
     if (volumes) {
         dataToSet.volumes = volumes;
     }
+    if (domain) {
+        dataToSet.domain = domain;
+    }
     await client.update('vessyl', 'resources', {name, owner: decoded.username}, {$set: dataToSet});
+    if(!domain) {
+        return c.json({success: true, message: 'Resource updated'})
+    }
+    // post request to process.env.PROXY_URI/get
+    const getProxyData = await fetch(`${process.env.PROXY_URI}/get`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+    const proxyData = await getProxyData.json();
+    if (proxyData.error) {
+        return c.json({error: 'Failed to update proxy, resource updated'});
+    }
+    // check if domain is already in proxy
+    const domainExists = proxyData.config.includes(domain);
+    if (domainExists) {
+        await fetch(`${process.env.PROXY_URI}/delete`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({domain})
+        });
+    }
+    const portToUse = ports[0].split(':')[0];
+    const addProxyData = await fetch(`${process.env.PROXY_URI}/new`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({domain, port: portToUse})
+    });
+    const addProxyJson = await addProxyData.json();
+    if (addProxyJson.error) {
+        return c.json({error: 'Failed to update proxy, resource updated'});
+    }
     return c.json({success: true, message: 'Resource updated'})
 });
 
