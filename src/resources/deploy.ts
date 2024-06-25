5646079f9e44cea7d3af8d1e92ff607cbca83ba0
Ben Thomas <benjamint08@gmail.com>
2024-06-25 10:51:18 +0100

    Deploys now go in vessyl-bridge

--- src/resources/deploy.ts
+++ src/resources/deploy.ts
@@ -43,7 +43,7 @@ app.post('/', async (c) => {
     if(userPat) {
         patToAdd = ` -e GITHUB_PAT=${userPat} -e GITHUB_USERNAME=${user.githubJson.username} `;
     }
-    let buildCommand = `docker run --rm --pull always --name DEPLOY-${cleanName} -v /var/run/docker.sock:/var/run/docker.sock -v /var/run/docker.sock:/var/run/docker.sock -e TYPE=${type} -e REPO_NAME=${repo_name}${patToAdd}ghcr.io/vessylapp/vessyl-buildenv:latest`
+    let buildCommand = `docker run --rm --pull always --network vessyl-bridge --name DEPLOY-${cleanName} -v /var/run/docker.sock:/var/run/docker.sock -v /var/run/docker.sock:/var/run/docker.sock -e TYPE=${type} -e REPO_NAME=${repo_name}${patToAdd}ghcr.io/vessylapp/vessyl-buildenv:latest`
     console.log(buildCommand);
     return streamText(c, (stream) => {
         return new Promise((resolve, reject) => {
