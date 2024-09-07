23a0a5495683f9e9a1460ca27dadeac8e372ea4f
benjamint08 <benjamint08@gmail.com>
2024-09-07 17:57:00 +0100

    0.0.60 restart proxy

--- src/admin.ts
+++ src/admin.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono'
 import getProxy from './admin/getProxy'
 import saveProxy from './admin/saveProxy'
+import restartProxy from "./admin/restartProxy";
 
 const app = new Hono()
 
 app.route('/getproxy', getProxy);
 app.route('/saveproxy', saveProxy);
+app.route('/restartproxy', restartProxy);
 
 export default app;
---
+++ src/admin/restartProxy.ts
@@ -0,0 +1,39 @@
+import { Hono } from 'hono'
+import jwt from 'jsonwebtoken'
+import MongoService from '../../structures/mongodb'
+import {exec, spawn} from "child_process";
+import { promisify } from 'util';
+const execAsync = promisify(exec);
+
+const app = new Hono()
+
+app.post('/', async (c) => {
+    const data = await c.req.text();
+    const body = JSON.parse(data);
+    const {token, url} = body;
+    const client = MongoService.getInstance();
+    const jwtSecret = await client.findOne('vessyl', 'settings', {jwtSecret: {$exists : true}});
+    if (!jwtSecret) {
+        return c.text('JWT Secret not found');
+    }
+    let decoded : any = {};
+    try {
+        decoded = jwt.verify(token, jwtSecret.jwtSecret);
+    } catch (err) {
+        return c.text('Invalid token');
+    }
+    const user = await client.findOne('vessyl', 'users', {username: decoded.username});
+    if (!user) {
+        return c.text('User not found');
+    }
+    if(user.admin === false || user.admin === undefined) {
+        return c.json({error: 'User is not an admin'});
+    }
+    c.json({success: 'Restarting proxy...'});
+    const restartCommand = `docker restart vp`
+    setTimeout(() => {
+        const restartProcess = spawn(restartCommand, { shell: true });
+    }, 1000);
+})
+
+export default app;
